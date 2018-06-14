51e016a992ef571ac41dce875b16eb884a3528a1
Tom Carrio <Thomas.Carrio@Ally.com>
2018-06-14 10:27:33 -0400

    Removed redundant variable settings in child class

--- src/domain/parking/ValetTicket.java
+++ src/domain/parking/ValetTicket.java
@@ -2,11 +2,9 @@ package domain.parking;
 
 public class ValetTicket extends ParkingTicket {
 	
-	boolean hasFreeDayRule = true;
 	int hrRate = 5;
 	int freeTime = 0;
 	int maxDaily = 50;
-	int freeDayEvery = 7;
 	
 	public ValetTicket(int days, int hours, int minutes) {
 		super(days, hours, minutes);
